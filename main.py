b4e2a66a85dd9ad02d1bfafa7c18461ec06d847d
4ebyrek <vasya-korn71@mail.ru>
2021-10-11 09:34:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,91 @@
 import numpy as np
 
-print("Введите количество столбцов матрицы 1: ")
-j = int(input())
-
-print("Введите количество строк матрицы 1: ")
-i = int(input())
-
-ii = i
-jj = j
-
-Matrix1 = np.arange((i*j)).reshape(i, j)
-print(Matrix1)
-
-#------------------------------------------#
-
-print("Введите количество столбцов матрицы 2: ")
-j1 = int(input())
-
-print("Введите количество строк матрицы 2: ")
-i1 = int(input())
-
-ii1 = i1
-jj1 = j1
+print("Выберите функцию: ")
+print("1 - Матрица на матрицу")
+print("2 - Матрица на вектор")
+print("3 - Вектор на матрицу")
 
 
-Matrix2 = np.arange(i1*j1).reshape(i1, j1)
-print(Matrix2)
+n = int(input())
+
+def MatrixXMatrix():
+    print("Введите количество столбцов матрицы 1: ")
+    j = int(input())
+
+    print("Введите количество строк матрицы 1: ")
+    i = int(input())
+
+    Matrix1 = np.random.randint(20, size=(i, j))
+    print(Matrix1)
+
+    # ------------------------------------------#
+
+    print("Введите количество столбцов матрицы 2: ")
+    j1 = int(input())
+
+    print("Введите количество строк матрицы 2: ")
+    i1 = int(input())
+
+    Matrix2 = np.random.randint(20, size=(i1, j1))
+    print(Matrix2)
+
+    result = Matrix1.dot(Matrix2)
+    print("Результат умножения матриц: \n", result)
 
 
-result = Matrix1.dot(Matrix2)
-print("Результат умножения матриц: \n", result)
+def MatrixXVec():
 
 
+    print("Введите количество столбцов матрицы: ")
+    j = int(input())
+
+    print("Введите количество строк матрицы: ")
+    i = int(input())
+
+    Matrix = np.random.randint(20, size=(i, j))
+    print(Matrix, end="\n")
+
+    print("Введите количество элементов вектора: ")
+    ii = int(input())
+
+    Vector = np.random.randint(20, size=(ii, 1))
+    print(Vector, end="\n")
+
+    print()
+    if(ii == j):
+        result = Matrix.dot(Vector)
+        print(result, end="\n")
+    else:
+        print("Количество столбцов матрицы не равно количеству элементов вектора")
+
+def VecXMatrix():
+
+    print("Введите количество элементов вектора: ")
+    jj = int(input())
+
+    Vector = np.random.randint(20, size=(1, jj))
+    print(Vector, end="\n")
+
+    print("Введите количество столбцов матрицы: ")
+    j = int(input())
+
+    print("Введите количество строк матрицы: ")
+    i = int(input())
+
+    Matrix = np.random.randint(20, size=(i, j))
+    print(Matrix, end="\n")
+
+
+    print()
+    if(i == jj):
+        result = Vector.dot(Matrix)
+        print(result, end="\n")
+    else:
+        print("Количество строк матрицы не равно количеству элементов вектора")
+
+if (n == 1):
+    MatrixXMatrix()
+elif (n == 2):
+    MatrixXVec()
+elif (n == 3):
+    VecXMatrix()
